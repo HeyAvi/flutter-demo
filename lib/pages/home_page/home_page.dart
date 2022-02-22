d0151d50f7e72ad193535189bc3429e566c1ea70
avinash <avinashjhaaaa@gmail.com>
2022-02-22 23:55:59 +0530

    ready to launch

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:demo/models/user_model.dart';
+import 'package:demo/pages/user_details/user_details_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +12,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  UserModel? _userModel;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,12 +21,24 @@ class _HomePageState extends State<HomePage> {
         title: const Text('Home'),
         actions: [
           IconButton(
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut();
-                Navigator.pushNamedAndRemoveUntil(
-                    context, '/login', (route) => false);
-              },
-              icon: Icon(Icons.logout))
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.pushNamedAndRemoveUntil(
+                  context, '/login', (route) => false);
+            },
+            icon: const Icon(Icons.logout),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (builder) => UserDetailsUpdate(
+                            userModel: _userModel,
+                          )));
+            },
+            icon: const Icon(Icons.edit),
+          ),
         ],
       ),
       body: Center(
@@ -33,8 +50,44 @@ class _HomePageState extends State<HomePage> {
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Column(
-                children: const [
-                  Text('data'),
+                children: [
+                  const Text('\nYour Data Starts From Data Here\n'),
+                  StreamBuilder<QuerySnapshot>(
+                    stream: FirebaseFirestore.instance
+                        .collection('Users')
+                        .snapshots(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.hasError) {
+                        return const Text('Something went wrong');
+                      }
+
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Text("Loading");
+                      }
+
+                      return ListView(
+                        shrinkWrap: true,
+                        children: snapshot.data!.docs
+                            .map((DocumentSnapshot document) {
+                          Map<String, dynamic> data =
+                              document.data()! as Map<String, dynamic>;
+
+                          UserModel userModel =
+                              UserModel.fromJson(mapValue: data);
+                          _userModel = userModel;
+                          return Column(
+                            children: [
+                              Text(userModel.name),
+                              Text(userModel.email),
+                              Text(userModel.photoUrl ?? ''),
+                              Text(userModel.location),
+                            ],
+                          );
+                        }).toList(),
+                      );
+                    },
+                  )
                 ],
               ),
             ),
